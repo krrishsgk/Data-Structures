a5c5415b747e9b8b1ce69c17e5ae8678fffd964e
Gopalakrishnan <krrish.sgk@gmail.com>
2020-04-01 19:33:59 -0400

    adding seq search

--- 6.3-SeqSearch.py
+++ 6.3-SeqSearch.py
@@ -1,12 +1,12 @@
 def seqSearch(input, value):
     pos = 0
     found = False
-    if pos < size(input) and not found:
+    while pos < len(input) and not found:
          if input[pos] == value:
              found = True
          else:
              pos = pos + 1
-     return found
+    return found
 
 testlist = [1, 2, 32, 8, 17, 19, 42, 13, 0]
-print(sequentialSearch(testlist, 3))
+print(seqSearch(testlist, 12))
